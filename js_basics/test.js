1a560182042c5c81979dbe41944cb30dca5c9d13
Bigyan Gadtaula <advanceskillz1234@gmail.com>
2025-04-24 15:11:23 +0000

    objects

--- js_basics/test.js
+++ js_basics/test.js
@@ -1,1 +1,17 @@
 console.log("Test please")
+
+//Objects
+const myobject = {
+name : "Bigyan Gadtaula",
+status : "Agressively learning "
+}
+console.log(myobject.status)
+console.log(myobject["status"])
+
+myobject.gretting = function(){
+    console.log(`Hello, ${this.name}`)
+}
+
+
+console.log(myobject)
+myobject.gretting()
